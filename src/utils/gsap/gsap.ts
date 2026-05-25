7193b4911acf872d249a242e4553ebfefa1ff0cc
gulbin-dev <gulbindev@gmail.com>
2026-05-25 11:13:59 +0800

    feat: added gsap plugins and responsive queries

---
+++ src/utils/gsap/gsap.ts
@@ -0,0 +1,13 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const mediaQueries = {
+  desktop: "(min-width: 1024px)",
+  tablet: "(min-width: 768px) and (max-width: 1023px)",
+  mobile: "(max-width: 767px)",
+};
+
+export { gsap, ScrollTrigger, useGSAP, mediaQueries };
