ccd63c70db0636e4b88d83c89c1ed9128bee2e34
gulbin-dev <gulbindev@gmail.com>
2026-05-27 18:15:07 +0800

    chore: improve error handling on failed image fetch
    refractor: added 1 card for the UI

--- src/components/CardCategoryContainer.tsx
+++ src/components/CardCategoryContainer.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import { Category, Response } from "@utils/types";
 import { use } from "react";
 import Link from "next/link";
-import { SecondaryButton } from "@/components/UI/Button";
 import ComponentError from "@components/UI/ComponentError";
+import { useState } from "react";
+
 const CardCategory = ({
   name,
   image,
@@ -15,18 +16,27 @@ const CardCategory = ({
   image: string;
   slug: string;
 }) => {
+  const [imgSrc, setImgSrc] = useState(image);
+  const sourceNormalizer = imgSrc.includes("https")
+    ? imgSrc
+    : imgSrc.includes("image-not-found.webp")
+      ? imgSrc
+      : `https://${imgSrc}`;
   return (
     <Link
       href=""
       className="relative rounded-2xl bg-secondary min-h-20 flex flex-col gap-1 pb-2"
     >
       <Image
-        src={image}
+        src={sourceNormalizer}
         alt={name}
-        onError={(e) => (e.currentTarget.src = "/image-not-found.webp")}
+        onError={(e) => {
+          e.currentTarget.onerror = null;
+          setImgSrc("/image-not-found.webp");
+        }}
         width={160}
         height={160}
-        className="object-contain rounded-t-2xl"
+        className="object-contain rounded-t-2xl min-h-20"
       />
       <h3 className="text-size-sm  font-bold text-white text-center">{name}</h3>
     </Link>
@@ -50,7 +60,7 @@ export default function CardCategoryContainer({
     );
   return (
     <div className="grid grid-cols-2 grid-auto-rows gap-3">
-      {response.data.slice(0, 5).map((category: Category) => (
+      {response.data.slice(0, 6).map((category: Category) => (
         <CardCategory
           key={category.id}
           name={category.name}
@@ -58,7 +68,6 @@ export default function CardCategoryContainer({
           slug={category.slug}
         />
       ))}
-      <SecondaryButton>See more</SecondaryButton>
     </div>
   );
 }
