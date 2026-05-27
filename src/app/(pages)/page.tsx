1cb73ede948db06221c33413083a4259e7321cbc
gulbin-dev <gulbindev@gmail.com>
2026-05-27 18:13:46 +0800

    perf: improve performance by suspending the slow components
    feat: add search modal

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,16 +1,19 @@
-import { SearchIcon } from "@utils/tabler-icons";
 import PromoBannerContainer from "./_components/PromoBannerContainer";
 import Hero from "./_components/Hero";
-import ProductsContainer from "./_components/ProductsContainer";
+import FeatureProductsContainer from "./_components/FeatureProductsContainer";
 import { fetchProducts } from "@server/fetch-products";
 import { fetchcategory } from "@server/fetch-category";
 import PageContentContainer from "@components/PageContentContainer";
-import ToggleThemeButton from "./_components/ToggleThemeButton";
 import SectionHeader from "@components/UI/SectionHeader";
-import CardCategoryContainer from "@components/UI/CardCategoryContainer";
-import { PrimaryButton } from "@/components/UI/Button";
+import CardCategoryContainer from "@components/CardCategoryContainer";
+import { PrimaryButton } from "@components/UI/Button";
+import { Suspense } from "react";
+import { SecondaryButton } from "@components/UI/Button";
+import SearchContainer from "@components/SearchContainer";
+
 export default function HomePage() {
   const getProducts = fetchProducts();
+
   const getCategories = fetchcategory();
   // since the third party API does not provide sorting or skipping,
   // I have to make a workaround
@@ -18,37 +21,49 @@ export default function HomePage() {
   const newProductSlice = [5, 10];
   return (
     <PageContentContainer>
-      <section className="px-3 max-w-180 pt-1 text-secondary-foreground">
-        <h1 className="text-size-xl font-bold mt-2">
+      <section className="max-w-180 pt-1 text-secondary-foreground">
+        <h1 className="text-size-xl font-bold mt-2 px-3">
           Top Online Shopping Store with ShopEase
         </h1>
-        <button className="flex gap-1.5 bg-accent-pink rounded-2xl text-black text-size-sm px-3 py-1 min-w-[80vw] mt-4 text-start">
-          <SearchIcon size={20} stroke={2} /> Black T-shirt
-        </button>
-
+        <SearchContainer />
         <PromoBannerContainer />
         <Hero />
       </section>
       <section className="mt-10">
         <SectionHeader>Top Products</SectionHeader>
         <div className="px-3 mt-4">
-          <ProductsContainer products={getProducts} slice={topProductSlice} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <FeatureProductsContainer
+              products={getProducts}
+              slice={topProductSlice}
+            />
+          </Suspense>
         </div>
       </section>
       <section className="mt-10">
         <SectionHeader>New Arrivals</SectionHeader>
         <div className="px-3 mt-4">
-          <ProductsContainer products={getProducts} slice={newProductSlice} />
+          <Suspense fallback={<div>Loading...</div>}>
+            <FeatureProductsContainer
+              products={getProducts}
+              slice={newProductSlice}
+            />
+          </Suspense>
         </div>
 
         <div className="mt-10">
           <SectionHeader>Categories</SectionHeader>
-          <div className="px-3 mt-4 ">
-            <CardCategoryContainer category={getCategories} />
+          <div className="px-3 mt-6">
+            <Suspense fallback={<div>Loading...</div>}>
+              <div className="flex flex-col gap-3">
+                <CardCategoryContainer category={getCategories} />
+                <SecondaryButton>See more</SecondaryButton>
+              </div>
+            </Suspense>
           </div>
         </div>
 
-        <div className="mt-10 flex flex-col bg-primary border-4 border-secondary text-black dark:text-white rounded-2xl text-size-md mx-3 p-3">
+        <div className="mt-8 flex flex-col bg-primary border-4 border-secondary text-black dark:text-white rounded-2xl text-size-md mx-3 p-3">
           <p>
             Get Alerted Before the Best Deals{" "}
             <span className="text-secondary dark:text-accent-yellow font-bold">
@@ -59,8 +74,6 @@ export default function HomePage() {
           <PrimaryButton style="mt-3">Subscribe</PrimaryButton>
         </div>
       </section>
-
-      {/* <ToggleThemeButton>Toggle Theme</ToggleThemeButton> */}
     </PageContentContainer>
   );
 }
