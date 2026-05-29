9c1770868ce14a4bc2d60ce828808163eadd4d60
gulbin-dev <gulbindev@gmail.com>
2026-05-29 15:26:08 +0800

    refractor: added footer icon navigations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,16 @@
 
 import { LogoImageDisplay } from "./LogoImageDisplay";
 import { useAppSelector } from "@/utils/redux-toolkit/typed-hooks";
+import { GithubIcon, WebsiteIcon, LinkedInIcon } from "@/utils/tabler-icons";
+import Link from "next/link";
 
+const IconSpan = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <span className="bg-accent-pink p-1 block text-white rounded-2xl">
+      {children}
+    </span>
+  );
+};
 export default function Footer() {
   const theme = useAppSelector((state) => state.theme.theme);
   return (
@@ -10,14 +19,41 @@ export default function Footer() {
       <div className="bg-primary w-fit p-1 rounded-br-2xl">
         <LogoImageDisplay theme={theme} />
       </div>
-
       <div className="mt-2 px-3">
-        <p className="">
+        <p className="text-size-sm">
           This e-commerce website is for demonstration only. Made by frontend
           developer Joshua Glenn R. Gulbin.
         </p>
       </div>
-      <p> &copy; ShopEase 2026.All Rights Reserved.</p>
+
+      <ul className="flex gap-2.5 pl-3 pt-3">
+        <li>
+          <Link href="/">
+            <IconSpan>
+              <WebsiteIcon size={38} />
+            </IconSpan>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <IconSpan>
+              <GithubIcon size={38} />
+            </IconSpan>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <IconSpan>
+              <LinkedInIcon size={38} />
+            </IconSpan>
+          </Link>
+        </li>
+      </ul>
+
+      <p className="text-size-sm text-center p-3">
+        {" "}
+        &copy; ShopEase 2026.All Rights Reserved.
+      </p>
     </footer>
   );
 }
